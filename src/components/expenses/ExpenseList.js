ad71b7133a3d2f798c6d8e616ec4cda246d35fe4
echomonk <erdaloylupinar@gmail.com>
2023-02-01 08:51:15 +0300

    upload started project

---
+++ src/components/expenses/ExpenseList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../ui/Card";
+
+function ExpenseList(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => (
+        <ExpenseItem
+          key={item.id}
+          date={item.date}
+          title={item.title}
+          amount={item.amount}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default ExpenseList;
